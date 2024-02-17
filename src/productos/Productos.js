96db962428e36513208720b2c8dd2029ab9ed7d6
Ignacio Diaz <informatica.diaz@gmail.com>
2024-02-16 21:32:43 -0300

    add product

--- src/productos/Productos.js
+++ src/productos/Productos.js
@@ -1,9 +1,12 @@
 // productos.js
 
 const productos = [
-    { id: 1, nombre: 'Camiseta', precio: 100, cantidad: 1 },
-    { id: 2, nombre: 'Pantalón', precio: 150, cantidad: 1 },
-    { id: 3, nombre: 'Zapatos', precio: 200, cantidad: 1 },
+  { id: 1, nombre: 'Camiseta', precio: 100, cantidad: 1 },
+  { id: 2, nombre: 'Pantalón', precio: 150, cantidad: 1 },
+  { id: 3, nombre: 'Zapatos', precio: 200, cantidad: 1 },
+  { id: 4, nombre: 'Medias', precio: 50, cantidad: 1 },
+  { id: 5, nombre: 'Medias', precio: 50, cantidad: 1 },
+  { id: 6, nombre: 'Medias',precio: 50, cantidad: 1 },
     // Agrega más productos según sea necesario
   ];
   
