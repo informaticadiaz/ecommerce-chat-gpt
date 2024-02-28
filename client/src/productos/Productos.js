f40bca45841b35da1fc0b53db01c211179e30aa2
Ignacio Diaz <informatica.diaz@gmail.com>
2024-02-28 19:12:58 -0300

    .

--- client/src/productos/Productos.js
+++ client/src/productos/Productos.js
@@ -1,6 +1,7 @@
 // productos.js
 
 const productos = [
+  {/* las imagenes se pueden guardar en https://cloudinary.com/ */},
   { id: 1, categoria: 'camisas', nombre: 'Camisa', precio: 100, cantidad: 1, img: '/zapatillas.jpg' },
   { id: 2, categoria: 'chombas', nombre: 'Chomba', precio: 150, cantidad: 1, img: '/zapatillas.jpg' },
   { id: 3, categoria: 'pantalones', nombre: 'Pantalon', precio: 200, cantidad: 1, img: '/zapatillas.jpg' },
